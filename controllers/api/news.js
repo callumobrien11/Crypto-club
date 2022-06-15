a45914dedd85a6f8cee46046af9b60edea56c81d
callumobrien <callum.obrien11@icloud.com>
2022-06-15 10:23:18 -0400

    second commit

--- controllers/api/news.js
+++ controllers/api/news.js
@@ -6,7 +6,7 @@ module.exports = {
 async function getNews(req, res) {
     console.log(req.params.tickers)
     const response = await fetch (
-        'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,blockchain&apikey=WR5E6F1M35Y55WNY'
+        'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,blockchain&apikey=demo'
     ) 
     const result = await response.json()
     res.json(result)
