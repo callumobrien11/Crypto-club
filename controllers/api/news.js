e91d74d08144e0d84eacc720b35bbed75510060e
callumobrien <callum.obrien11@icloud.com>
2022-06-16 16:53:12 -0400

    please work x4

--- controllers/api/news.js
+++ controllers/api/news.js
@@ -11,5 +11,6 @@ async function getNews(req, res) {
     const response = await axios.get (
         'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,blockchain&apikey=WR5E6F1M35Y55WNY'
     ) 
-    res.json(response)
+    console.log(response)
+    res.send(response)
 }
