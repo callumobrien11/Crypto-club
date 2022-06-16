6daed2735b3ed5898d47153c657e375bff99d323
callumobrien <callum.obrien11@icloud.com>
2022-06-16 16:43:17 -0400

    please word x2

--- controllers/api/news.js
+++ controllers/api/news.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 module.exports = {
     getNews
 }
@@ -6,10 +8,9 @@ module.exports = {
 async function getNews(req, res) {
     console.log(req.params.tickers)
     
-    const response = await fetch (
+    const response = await axios.get (
         'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,blockchain&apikey=WR5E6F1M35Y55WNY'
     ) 
     const result = await response.json()
     res.json(result)
-    return result
 }
